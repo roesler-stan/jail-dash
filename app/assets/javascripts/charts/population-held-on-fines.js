ac62b2cd9ea6b868c216d7b30387f607d2352cde
Mark Rossetti <mark.a.rossetti@gmail.com>
2017-09-22 17:14:57 -0700

    Styled KPI sidebar on "population" page

---
+++ app/assets/javascripts/charts/population-held-on-fines.js
@@ -0,0 +1,10 @@
+class HeldOnFinesChart extends TimeseriesChart {
+  constructor(args) {
+    super(args);
+    this.args = args;
+  }
+
+  render(targetElementSelector, opts) {
+    this.base_render(targetElementSelector, opts);
+  }
+}
