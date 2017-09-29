179dd1004d376798d00768b0f795d1ab77494bf3
Mark Rossetti <mark.a.rossetti@gmail.com>
2017-09-29 11:40:31 -0700

    Added KPI bars to population page

--- app/assets/javascripts/pages/population.js
+++ app/assets/javascripts/pages/population.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
   $('.js-accordion-trigger').click(function(e){
     e.preventDefault();
-    $(this).parent().toggleClass('is-expanded');
+    $(this).parent().parent().toggleClass('is-expanded');
 
     // only redraw a chart when opening the accordion
-    if ($(this).parent().hasClass('is-expanded')) {
+    if ($(this).parent().parent().hasClass('is-expanded')) {
       $(this).trigger('drawchart-'+$(this).data('chart'))
     }
   });
