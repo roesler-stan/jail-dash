d809b4eb384f7ca321a5dd840ce45249218ceef3
Mark Rossetti <mark.a.rossetti@gmail.com>
2017-10-04 15:35:04 -0700

    Show appropriate time labels for charts, and update on date selection

--- app/assets/javascripts/datepicker-init.js
+++ app/assets/javascripts/datepicker-init.js
@@ -7,16 +7,13 @@ $(document).ready(function () {
 
     const start = moment($el.parent().data('default-time-start'), 'YYYYMMDD')
     const end = moment($el.parent().data('default-time-end'), 'YYYYMMDD')
-
-    function cb(start, end) {
-      $el.find('span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
-    }
+    const interval = $el.parent().data('default-time-interval')
 
     const allRanges = {
       'This month': [moment().startOf('month'), moment()],
       'Last month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')],
       'This quarter': [moment().startOf('quarter'), moment()],
-      'Last quarter': [moment().subtract(1, 'quarter').startOf('quarter'), moment().subtract(1, 'quarter').startOf('quarter')],
+      'Last quarter': [moment().subtract(1, 'quarter').startOf('quarter'), moment().subtract(1, 'quarter').endOf('quarter')],
       'This year': [moment().startOf('year'), moment()],
       'Last year': [moment().subtract(1, 'year').startOf('year'), moment().subtract(1, 'year').endOf('year')],
       'Weekly': [moment().subtract(9, 'week').startOf('week'), moment().subtract(1, 'week').endOf('week')],
@@ -33,10 +30,27 @@ $(document).ready(function () {
         customRangeLabel: 'Custom...'
       },
       alwaysShowCalendars: false, // TODO: this doesn't seem to be working properly?
-    }, cb);
+    }, setDateBanners);
 
-    cb(start, end);
+    function setDateBanners(start, end, periodLabel) {
+      const specificPeriods = ['Custom...','This month','Last month','This quarter','Last quarter','This year','Last year']
+
+      let text = '';
+
+      if (specificPeriods.includes(periodLabel)) {
+        text = periodLabel
+        text += ' ('+start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY')+')'
+      } else { // is an interval like 'Quarterly'
+        text = periodLabel
+        text += ' (last 8 periods)'
+      }
+
+      $el.find('span').html(periodLabel);
+      $el.parent().siblings('.chart_subtitle').html(text);
+    }
+
+    setDateBanners(start, end, interval);
   });
 
-    
+
 });
